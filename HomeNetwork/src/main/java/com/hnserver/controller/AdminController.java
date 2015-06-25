3037efadcba7e6096bab872387b0bdba5dffa3ec
mikiejones <mikiej_boy@hotmail.com>
2015-06-25 20:53:35 +0100

    Successfully Added user to database
    user can now be added to database - the test code was copied across in
    order to facilitate the adding of user - this is duplicate code and as
    such a new object should be create in order to do this - or create an
    interface/abstract object to assist

--- HomeNetwork/src/main/java/com/hnserver/controller/AdminController.java
+++ HomeNetwork/src/main/java/com/hnserver/controller/AdminController.java
@@ -1,5 +1,7 @@
 package com.hnserver.controller;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -8,59 +10,65 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import UserPackage.JdbcUserControl;
 import UserPackage.User;
-
-
-
-
 
 @Controller
 public class AdminController {
 
 	@RequestMapping(value = "/adminpage/usersAdded", method = RequestMethod.POST)
-	public String addUser(ModelMap model, @ModelAttribute("users") User user, BindingResult res) 
-	{	
-	System.out.println("**********************Calling adduser page ***************");
-	String returnedUserName = user.getUserName();
-	String returnedUserPassword = user.getPassword();
-	String message = "";
+	public String addUser(ModelMap model, @ModelAttribute("users") User user,
+			BindingResult res) {
+		System.out
+				.println("**********************Calling adduser page ***************");
+		String returnedUserName = user.getUserName();
+		String returnedUserPassword = user.getPassword();
+		String message = "";
 		if (returnedUserName.equals(""))
 		{
 			System.out.println("Invalid username - add a User name");
-			model.addAttribute("message","Invalid username - add a User name");
-			  return "addusers";
+			model.addAttribute("message", "Invalid username - add a User name");
+			return "addusers";
 		}
-		else if (returnedUserPassword.equals("") || returnedUserPassword.length() < 5)
+		else if (returnedUserPassword.equals("")|| returnedUserPassword.length() < 5) 
 		{
-			//setup password standards - check with spring
+			// setup password standards - check with spring
 			System.out.println("No password chosen or too small");
-			
+
 			model.addAttribute("message", "No password chosen or too small");
-			  return "addusers";
+			return "addusers";
 		}
-		else
+		else 
 		{
+			// correctly eneterd name - add user
 			System.out.println("User added");
-				System.out.println("username: " + user.getUserName() + ", password: " + user.getPassword());
-			
+			System.out.println("username: " + user.getUserName()
+					+ ", password: " + user.getPassword());
+
+			// this needs to be in a object of its own - move laters once tested
+			ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+			JdbcUserControl userObject = (JdbcUserControl) context.getBean("userObject");
+			//User user1 = new User();
+			//user.setUserName(returnedUserName);
+			//user.setPassword(returnedUserPassword);
+			userObject.insert(user);
 		}
-		
-	   return "redirect:addUsers";
+
+		return "redirect:addUsers";
 	}
 
-	   @RequestMapping("/adminpage/addUsers")
-	    public ModelAndView showAddUserPage() {
-	         
-	    	//needs command - 
-	        return new ModelAndView("adduser", "command", new User());
-	    }
-	
-	
-	
-	@RequestMapping("/adminRedirect")
-	public String redirectToAdmin() {	
-	System.out.println("**********************Calling Admin redirect page ***************");
+	@RequestMapping("/adminpage/addUsers")
+	public ModelAndView showAddUserPage() {
 
-	return "redirect:adminpage";
+		// needs command - apparently according to spring docs requires command
+		return new ModelAndView("adduser", "command", new User());
+	}
+
+	@RequestMapping("/adminRedirect")
+	public String redirectToAdmin() {
+		System.out
+				.println("**********************Calling Admin redirect page ***************");
+
+		return "redirect:adminpage";
 	}
 }
--- HomeNetwork/src/test/java/testDatabase/Tester.java
+++ HomeNetwork/src/test/java/testDatabase/Tester.java
@@ -17,24 +17,25 @@ public class Tester {
 
 	public static void main(String[] args) {
 		      ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-
+		      //ApplicationContext context = new ClassPathXmlApplicationContext("/dispatcherservlet-servlet.xml");
+		      
 		      JdbcUserControl userObject = (JdbcUserControl)context.getBean("userObject");
 		      
 		      
 		      //Add new user to database
-		      
-//		      System.out.println("------Records Creation--------" );
-//		      User user1 = new User();
-//		      user1.setUserName("dave");
-//		      user1.setPassword("dave@123");		      
-//		      userObject.insert(user1);
+	      
+		      System.out.println("------Records Creation--------" );
+		      User user1 = new User();
+		      user1.setUserName("dave");
+		      user1.setPassword("dave@123");		      
+		      userObject.insert(user1);
 		      
 		      
 		      //delete user from database
 		      
-		      System.out.println("------Records Deletion--------" );
-		      User temp = userObject.getuserByName("dave");
-		      userObject.delete(temp);
+//		      System.out.println("------Records Deletion--------" );
+//		      User temp = userObject.getuserByName("dave");
+//		      userObject.delete(temp);
 		      
 
 		   }
